fec43511d13accf31ba6330b77062f59b76da4e3
Quartz <2360527568@qq.com>
2019-04-01 00:59:38 +0800

    feat: 完善获取页面源码函数

--- headless.js
+++ headless.js
@@ -18,30 +18,36 @@ function sleep(timeout) {
     })
 }
 
-async function getPageSource(url, tempPath) {
-    tempPath = path.resolve(tempPath)
+function getPageSource(url, tempPath) {
+    return new Promise(async function(resolve, reject) {
+        tempPath = path.resolve(tempPath)
 
-    // 检查是否有 temp 文件夹
-    if (!fs.existsSync(tempPath)) fs.mkdirSync(tempPath)
+        // 检查是否有 temp 文件夹
+        if (!fs.existsSync(tempPath)) fs.mkdirSync(tempPath)
 
-    // 实例化浏览器驱动器
-    let browser = new Builder()
-    .forBrowser('chrome')
-    .setChromeOptions(chromeOptions)
-    .build()
+        // 实例化浏览器驱动器
+        let browser = new Builder()
+            .forBrowser('chrome')
+            .setChromeOptions(chromeOptions)
+            .build()
 
-    // 等待页面源码加载完毕
-    await browser.get(url)
-    let pageSource = await browser.getPageSource()
-    let pageTitle = await browser.getTitle()
-    browser.quit()
+        // 等待页面源码加载完毕
+        await browser.get(url)
+        let pageSource = await browser.getPageSource()
+        let pageTitle = await browser.getTitle()
+        let pagePath = tempPath + '\\' + pageTitle + '.html'
+        browser.quit()
 
-    // 写入 html 文件到 temp
-    fs.writeFileSync(
-        tempPath + '\\' + pageTitle + '.html',
-        pageSource)
+        // 写入 html 文件到 temp
+        fs.writeFileSync(pagePath, pageSource)
 
-    console.log(tempPath + '\\' + pageTitle + '.html')
+        console.log(pagePath)
+
+        fs.readFile(pagePath, function(err, data) {
+            if (err) reject(err)
+            else resolve(data.toString())
+        })
+    })
 }
 
 (async function () {
@@ -53,6 +59,7 @@ async function getPageSource(url, tempPath) {
         'https://www.jianshu.com/u/30f796232c22'
     ]
     urlList.forEach(async url => {
-        getPageSource(url, './temp/')
+        let source = await getPageSource(url, './temp/')
+        console.log(source)
     })
 })()
